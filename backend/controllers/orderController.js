b8175663b32f1d6dde0ef913bff21c7cc7c6b8e9
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-11-13 13:55:23 -0600

    order details for backend, orderScreen frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -29,5 +29,20 @@ const addOrderItems = asyncHandler(async(req, res) => {
 
 })
 
-export { addOrderItems}
+
+// description: Get order by Id
+// route: GET/api/orders/:id
+// access: private route
+const getOrderById = asyncHandler(async(req, res) => {
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
+  if(order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+
+})
+
+export { addOrderItems, getOrderById }
 
