9a5a5d37f3adab5ac04800ad08cd2aa73e5a7ad8
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-11-14 20:53:20 -0600

    admin update product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -32,7 +32,7 @@ const ProductListScreen = ({ history, match }) => {
     }
 
     if(successCreate) {
-      history.push(`/admin/products/${createdProduct._id}/edit}`)
+      history.push(`/admin/product/${createdProduct._id}/edit`)
     } else {
       dispatch(listProducts())
     }
