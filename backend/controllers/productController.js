01335a4b028779fc27692ea26c6933f4d45e1fd8
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-11-14 18:42:09 -0600

    admin create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -40,4 +40,48 @@ const deleteProduct = asyncHandler(async(req, res) => {
   }
 })
 
-export { getProducts, getProductById, deleteProduct }
+// description: Create product
+// route: POST/api/products
+// access: private admin route
+const createProduct = asyncHandler(async(req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description'
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+
+// description: Update product
+// route: PUT/api/products/:id
+// access: private admin route
+const updateProduct = asyncHandler(async(req, res) => {
+  const {name, price, description, image, category, countInStock} = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if(product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.category = category
+    product.countInStock = countInStock
+
+  const updatedProduct = await product.save()
+  res.json(updatedProduct)
+
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 const router = express.Router()
 import { admin, protect } from '../middleware/authMiddleware.js'
-import { getProductById, getProducts, deleteProduct } from '../controllers/productController.js'
+import { getProductById, getProducts, deleteProduct, updateProduct, createProduct } from '../controllers/productController.js'
 
 
 
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 export default router
